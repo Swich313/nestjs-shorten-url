5c5ac3bf5adc2b232b43c48a6b894bc8a168b16f
Swich <andruha313@gmail.com>
2023-06-14 15:59:02 +0300

    port fix

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  const port = process.env.PORT ? process.env.PORT : 4000;
+  const port = parseInt(process.env.PORT) || 3000;
   await app.listen(port);
   logger.log(`Application listening port: ${port}`);
 }
