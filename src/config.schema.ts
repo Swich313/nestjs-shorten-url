1d19405f0929c5eada0a42cd60b114de279997cb
Andrey <95275077+Swich313@users.noreply.github.com>
2023-06-14 22:03:20 +0300

    Update config.schema.ts

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -2,8 +2,8 @@ import * as Joi from '@hapi/joi';
 
 export const configValidationSchema = Joi.object({
   PORT: Joi.number().default(8000),
-  MONGODB_SUPER_USER_NAME:  Joi.string().required(),
-  MONGODB_SUPER_USER_PASSWORD: Joi.string().required(),
+  // MONGODB_SUPER_USER_NAME:  Joi.string().required(),
+  // MONGODB_SUPER_USER_PASSWORD: Joi.string().required(),
   MONGODB_USER_NAME:  Joi.string().required(),
   MONGODB_USER_PASSWORD: Joi.string().required(),
   MONGODB_CLUSTER: Joi.string().required(),
